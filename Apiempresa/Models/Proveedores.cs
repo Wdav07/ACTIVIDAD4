28dcee7644c85064d7ff61dda1774fb14783d440
David Obando <dave7.obando@gamil.com>
2024-10-29 23:29:59 -0600

    Dave

---
+++ Apiempresa/Models/Proveedores.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiEmpresa.Models;
+public class Proveedores{
+    [Key]
+    public Int32 id_proveedores { get; set; }
+    public string? nit { get; set; }
+    public string? proveedor { get; set; }
+    public string? direccion { get; set; }
+    public string? razon_social { get; set; }
+    public DateTime? fecha_creacion { get; set; }
+    
+}
